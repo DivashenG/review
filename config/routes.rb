eb960776d634171e0b777d1a87c3aafe6a3fcc81
Divashen <39258459+DivashenG@users.noreply.github.com>
2018-05-20 14:25:33 +0200

    Create user controller (#3)
    * Add initial iteration of user controller structure
    
    * Add initial user controller structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/index'
 
+  resources :users
+
   root 'home#index'
 end
